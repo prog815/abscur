b87293669090aa7c3efed2b1ba80a0598da99bee
prog815 <eavprog@gmail.com>
2020-04-24 09:25:14 +0500

    создали первую переменную

---
+++ abscur_data_url.py
@@ -0,0 +1,2 @@
+abscur_data_url = 'https://docs.google.com/spreadsheets/d/1_-cdNCIC6NgyRDj0zLJBaohXo-K8qCfxfDLbYq2JLhs/export?format=csv&id=1_-cdNCIC6NgyRDj0zLJBaohXo-K8qCfxfDLbYq2JLhs&gid=422799220'
+

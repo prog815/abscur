14d875dde21515b24a231155bcfc3556c17e0439
prog815 <eavprog@gmail.com>
2020-04-24 10:12:34 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='abscur',
+      version='0.01',
+      description='Библиотека доступа к данным проекта <Абсолютный курс>',
+      packages=['abscur'],
+      author_email='eavprog@gmail.com',
+      zip_safe=False)

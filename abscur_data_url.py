813a1fc6c00a897899c8c9e77a351363d9fd0803
prog815 <eavprog@gmail.com>
2020-05-02 21:34:30 +0500

    Delete abscur_data_url.py

--- abscur_data_url.py
+++
@@ -1,2 +0,0 @@
-abscur_data_url = 'https://docs.google.com/spreadsheets/d/1_-cdNCIC6NgyRDj0zLJBaohXo-K8qCfxfDLbYq2JLhs/export?format=csv&id=1_-cdNCIC6NgyRDj0zLJBaohXo-K8qCfxfDLbYq2JLhs&gid=422799220'
-

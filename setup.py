b56517ff3e3b5150ec6e36b9d189e8d0248f1068
prog815 <eavprog@gmail.com>
2020-05-02 21:00:00 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os.path import join, dirname
 
-setup(name='abscur',
-      version='0.01',
-      description='Библиотека доступа к данным проекта <Абсолютный курс>',
-      packages=['abscur'],
-      author_email='eavprog@gmail.com',
-      zip_safe=False)
+setup(
+    name='abscur',
+    version='0.01',
+    description='Библиотека доступа к данным проекта <Абсолютный курс>',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    author_email='eavprog@gmail.com',
+    zip_safe=False
+)

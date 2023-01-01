a6eff3fd3c835415de7decc75de9d6b5654f23d0
Енин Алексей <eavprog@gmail.com>
2023-01-01 22:05:14 +0500

    Kaggle Notebook | abscur_menu | Version 8

--- abscur-menu.py
+++ abscur-menu.py
@@ -1,14 +1,15 @@
 # %% [code]
 def print_menu():
     from IPython.display import Markdown, display
-
-    def print_link(text,link):
-        md_code = f"- [{text}]({link})"
-        display(Markdown(md_code))
-
-    display(Markdown("# Оглавление проекта \"Абсолютный валютный курс\""))
-    print_link("Источник данных с абсолютными курсами","https://www.kaggle.com/code/eavprog/abscur-data")
-    print_link("Последние абсолютные курсы","https://www.kaggle.com/code/eavprog/abscur-posled-kurs")
-    print_link("Абсолютные курсы голубых фишек Московской биржи","https://www.kaggle.com/code/eavprog/mmvb-abscur-blue-fishki")
+    
+    text = """
+    # Оглавление проекта \"Абсолютный валютный курс\" 
+    - [Источник данных с абсолютными курсами](https://www.kaggle.com/code/eavprog/abscur-data) 
+    - [Последние абсолютные курсы](https://www.kaggle.com/code/eavprog/abscur-posled-kurs)
+    - [Абсолютные курсы голубых фишек Московской биржи](https://www.kaggle.com/code/eavprog/mmvb-abscur-blue-fishki)
+    """
+    
+    display(Markdown(text))
+    
     
   

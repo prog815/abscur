c7a89e50b1a0ce45079e84e7db5b3501f6b39f92
Енин Алексей <eavprog@gmail.com>
2023-01-01 22:21:06 +0500

    Kaggle Notebook | abscur_menu | Version 11

--- abscur-menu.py
+++ abscur-menu.py
@@ -3,9 +3,23 @@ def print_menu():
     from IPython.display import Markdown, display
     
     text = """# Оглавление проекта \"Абсолютный валютный курс\" 
+
+## Представительства проекта:
+[сайт](http://www.abscur.ru)
+[ВКонтакте](https://vk.com/abscur)
+[WhatsUp](https://chat.whatsapp.com/KrNJXAKizPxDlDSjYfdpQs)
+[Viber](https://invite.viber.com/?g2=AQB63y7Cm%2BNJK0tNZW%2F9kI00M6Wr8HuZ7XeamzN4jZCve8Aq9%2FiD3DY56Eqcf7dc)
+[Telegram](https://t.me/AbsCur)
+[Одноклассники](https://ok.ru/group/53547043520717)
+[GitHub](https://github.com/prog815/abscur)
+[YouTube](https://www.youtube.com/playlist?list=PLG1V24vhSKo6MamGtgm2oTkDZHeHBHNFW)
+
+## Расчеты на Kaggle:
 - [Источник данных с абсолютными курсами](https://www.kaggle.com/code/eavprog/abscur-data) 
 - [Последние абсолютные курсы](https://www.kaggle.com/code/eavprog/abscur-posled-kurs)
-- [Абсолютные курсы голубых фишек Московской биржи](https://www.kaggle.com/code/eavprog/mmvb-abscur-blue-fishki)"""
+- [Рейтинг абсолютной доходности](https://www.kaggle.com/eavprog/abscur-reyting-dohod)
+- [Абсолютные курсы голубых фишек Московской биржи](https://www.kaggle.com/code/eavprog/mmvb-abscur-blue-fishki)
+"""
     
     display(Markdown(text))
     

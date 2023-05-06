4478ea8eba39eb6e5d838a4a6afb021512f056e6
Енин Алексей <eavprog@gmail.com>
2023-05-06 09:20:31 +0500

    Kaggle Notebook | abscur_menu | Version 32

--- abscur-menu.py
+++ abscur-menu.py
@@ -1,3 +1,4 @@
+# %% [code]
 def print_menu():
     from IPython.display import Markdown, display
     
@@ -6,17 +7,17 @@ def print_menu():
 ![Проект \"Абсолютный валютный курс\"](https://i.ibb.co/cL0D27B/abscur-znak.jpg)
 
 ## Представительства проекта:
-[**БЛОГ**](http://www.abscur.ru) - 
-[**VK**](https://vk.com/abscur) - 
-[**WhatsApp**](https://chat.whatsapp.com/KrNJXAKizPxDlDSjYfdpQs) - 
-[**Viber**](https://invite.viber.com/?g2=AQB63y7Cm%2BNJK0tNZW%2F9kI00M6Wr8HuZ7XeamzN4jZCve8Aq9%2FiD3DY56Eqcf7dc) - 
-[**Telegram**](https://t.me/AbsCur) - 
-[**ОК**](https://ok.ru/group/53547043520717) - 
-[**GitHub**](https://github.com/prog815/abscur) - 
-[**YouTube**](https://www.youtube.com/playlist?list=PLG1V24vhSKo6MamGtgm2oTkDZHeHBHNFW) - 
-[**Smart-Lab**](https://smart-lab.ru/my/eavpred/) - 
-[**VC.ru**](https://vc.ru/s/1826351-absolyutnyy-kurs-valyut) - 
-[**Kaggle**](https://www.kaggle.com/code/eavprog/abscur-about)
+- [**БЛОГ**](http://www.abscur.ru) 
+- [**VK**](https://vk.com/abscur)
+- [**WhatsApp**](https://chat.whatsapp.com/KrNJXAKizPxDlDSjYfdpQs)
+- [**Viber**](https://invite.viber.com/?g2=AQB63y7Cm%2BNJK0tNZW%2F9kI00M6Wr8HuZ7XeamzN4jZCve8Aq9%2FiD3DY56Eqcf7dc)
+- [**Telegram**](https://t.me/AbsCur)
+- [**ОК**](https://ok.ru/group/53547043520717)
+- [**GitHub**](https://github.com/prog815/abscur)
+- [**YouTube**](https://www.youtube.com/@eavprog8/community)
+- [**Smart-Lab**](https://smart-lab.ru/my/eavpred/)
+- [**VC.ru**](https://vc.ru/s/1826351-absolyutnyy-kurs-valyut)
+- [**Kaggle**](https://www.kaggle.com/code/eavprog/abscur-about)
 
 ## Абсолютные валютные курсы:
 - [О проекте](https://www.kaggle.com/eavprog/abscur-about)
